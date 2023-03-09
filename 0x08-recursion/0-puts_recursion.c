c0d4acb5f8e6d035bfd2480f71797e4088d180f0
okohjrofficial <okohmecedezjosh@gmail.com>
2023-03-09 15:45:32 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"                                                                                                                       
-
 /**                                                                                                                                     
  * _puts_recursion - Prints a string, followed by a new line.                                                                           
  * @s: The string to be printed.                                                                                                        
@@ -8,10 +7,9 @@ void _puts_recursion(char *s)
 {                                                                                                                                       
         if (*s)                                                                                                                         
         {                                                                                                                               
-                _putchar(*s);                                                                                                           
-                _puts_recursion(s + 1);                                                                                                 
+        	_putchar(*s);                                                                                                           
+       		_puts_recursion(s + 1);                                                                                                 
         }                                                                                                                               
-
         else                                                                                                                            
-                _putchar('\n');                                                                                                         
+ 		 _putchar('\n');                                                                                                         
 }
